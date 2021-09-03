bce744b30d2d02f8f61370276a74d2b94ab7d4d9
Marlena Tsang <Marlena345@gmail.com>
2021-09-02 21:24:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+/*
+for(let age=30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+  function writeCards(names, surprise) {
+  let message = [];
+  for(let i = 0; i < names.length; i++) {
+      message.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+   }
+   return message;
+  }
+
+  function countDown (postiveNumber) {
+      while(postiveNumber >= 0) {
+      console.log(postiveNumber);
+      postiveNumber -= 1;
+  }
+}
